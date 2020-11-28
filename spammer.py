965ef41587e280cb94da4a0b41f7fcc14514e5c9
AdRsh <71685388+adRajgit@users.noreply.github.com>
2020-11-29 01:05:59 +0530

    Add files via upload

---
+++ spammer.py
@@ -0,0 +1,25 @@
+import pyautogui, time
+time.sleep(2)
+f = open("bee- Barry?
+for word in f:
+    pyautogui.typewrite(word)
+    pyautogui.press("enter")
+     
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
